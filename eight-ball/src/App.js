074f077808129d0805d9559d68c66655f5c708ee
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-22 09:46:07 -0600

    Refactor eight ball code and delete unecessary CRA files

--- eight-ball/src/App.js
+++ eight-ball/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import EightBall from './EightBall';
 import './App.css';
 
--- eight-ball/src/EightBall.js
+++ eight-ball/src/EightBall.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import "./EightBall.css";
 import responses from './responses';
 
+const randSelection = (arr) => {
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
+
 // Component EightBall takes a prop 'answers' which is an array
 const EightBall = ({ answers=responses }) => {
   /**
@@ -10,23 +15,28 @@ const EightBall = ({ answers=responses }) => {
   /**
    * Need to:
    * 1. Use state to set an initial msg and color for the ball ("Think of a Question" and "black")
-   * 2. Add event listener (onClick) to the eight ball so that:
+   * 2. Add click event listener to the eight ball so that:
    *   * a random message/color will be chosen
    *   * it will update the state of the eightball with the new message and color.
    */
-  const genRandom = () => Math.floor(Math.random() * answers.length);
-  const getAnswer = () => {
-    const idx = genRandom();
-    setMsg(answers[idx].msg);
-    setColor(answers[idx].color);
-  }
+
+  // set initial state for eight ball
   const [ msg, setMsg ] = useState("Think of a Question");
   const [ color, setColor ] = useState("black");
+
+  const handleClick = () => {
+    const { msg, color } = randSelection(answers);
+    setMsg(msg);
+    setColor(color);
+  }
+
   return (
-    <div className="EightBall">
-      <div style={{backgroundColor: color}} onClick={getAnswer}className="EightBall-img">
-        <p className="EightBall-text">{msg}</p>
-      </div>
+    <div 
+      className="EightBall" 
+      style={{backgroundColor: color}} 
+      onClick={handleClick}
+    >
+      <p className="EightBall-text">{msg}</p>
     </div>
   )
 }
